84bea8c3643ee32231cbd34c52846da6252f9d67
Andreas Sekulski <andreas.sekulski@gmail.com>
2025-07-14 22:38:52 +0200

    Replace as ajax event to load data (#3)

--- src/pitstop/race_events/urls.py
+++ src/pitstop/race_events/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("api/events", views.ajax_events, name="api/events"),
 ]
--- src/pitstop/race_events/views.py
+++ src/pitstop/race_events/views.py
@@ -1,13 +1,14 @@
 from zoneinfo import ZoneInfo
-from asgiref.sync import sync_to_async
 from django.shortcuts import render
 from pitstop.service.formula_api import FormulaApi
 
-async def index(request):
+def index(request):
+    return render(request, "race_events.html")
 
-    schedule = await sync_to_async(FormulaApi.get_event_schedule)(2025, include_testing=False)
-    events = await sync_to_async(FormulaApi.get_all_events_from_year)(schedule)
-    driver_df, constructor_df = await sync_to_async(FormulaApi.get_standings)(schedule)
+def ajax_events(request):
+    schedule = FormulaApi.get_event_schedule(2025, include_testing=False)
+    events = FormulaApi.get_all_events_from_year(schedule)
+    driver_df, constructor_df = FormulaApi.get_standings(schedule)
 
     # Convert all session dates to the specified timezone
     zone = ZoneInfo("Europe/Berlin")
@@ -21,9 +22,9 @@ async def index(request):
         event.active = True
 
     context = {
-        'events': events,
-        'driver_standings': driver_df.to_dict(orient='records'),
-        'constructor_standings': constructor_df.to_dict(orient='records'),
+        "events": events,
+        "driver_standings": driver_df.to_dict(orient="records"),
+        "constructor_standings": constructor_df.to_dict(orient="records"),
     }
 
-    return render(request, "race_events.html", context)
+    return render(request, "events.html", context)
