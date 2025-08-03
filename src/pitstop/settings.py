39dfaa0cea74e11ab73dc5ae98e40554f23d6ffb
Andreas Sekulski <andreas.sekulski@gmail.com>
2025-08-03 15:20:47 +0200

    Missing staticfiles directory

--- src/pitstop/settings.py
+++ src/pitstop/settings.py
@@ -96,6 +96,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Fast F1 cache directory
 if DEBUG:
