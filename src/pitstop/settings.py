c13f305ee1ba29e076b592682295698c9d0ef289
Andreas Sekulski <andreas.sekulski@gmail.com>
2025-08-03 11:57:12 +0200

    Include favicon

--- src/pitstop/settings.py
+++ src/pitstop/settings.py
@@ -95,6 +95,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / STATIC_URL]
 
 # Fast F1 cache directory
 if DEBUG:
