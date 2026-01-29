ee9e04dd8d655bd877b6fc0049616e9f85f20a11
Andreas Sekulski <andreas.sekulski@gmail.com>
2026-01-29 23:38:58 +0100

    Upgrade all dependencies

--- src/pitstop/settings.py
+++ src/pitstop/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True  # pylint: disable=invalid-name
 if os.environ.get("DEBUG", default="True").lower() == "false":
-    DEBUG = False
+    DEBUG = False  # pylint: disable=invalid-name
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
